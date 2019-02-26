8fb7f13dc0c7106dd921feaefdd0f601d71da051
Richard Harris <richard@idestini.com>
2019-02-26 10:55:21 -0600

    Small edits

---
+++ simple.js
@@ -0,0 +1,33 @@
+let simple = new Comic("layout1", {
+    width: "800px",
+    cols: 4,
+    gutters: "15px",
+    panel: {
+        background: "#444",
+        height: "250px",
+        padding: "10px",
+        gutters: "15px"
+    },
+    panels: [{
+            width: 4
+        },
+        {
+            width: 2
+        },
+        {
+            width: 2
+        },
+        {
+            width: 1
+        },
+        {
+            width: 3
+        },
+        {
+            width: 2
+        },
+        {
+            width: 2
+        }
+    ]
+});
